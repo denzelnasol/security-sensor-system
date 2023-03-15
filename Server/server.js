42403d257a495fb05e2b792a4fd79b7edaa5c309
denzelnasol <denzelnasol@gmail.com>
2023-03-14 22:32:56 -0700

    modify capure.c and add node server implementation to stream on webpage

--- Server/server.js
+++ Server/server.js
@@ -1,2 +1,58 @@
-import stream from "./lib/stream.js";
-stream.listen();
+// import stream from "./lib/stream.js";
+// stream.listen();
+const startRouter = require('./routers/page.js');
+const child = require('child_process');
+
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const { SERVER_PORT: port = 3000 } = process.env;
+
+app.use('/', startRouter);
+
+io.on('connection', (socket) => {
+
+    console.log('A user connected');
+
+    let ffmpeg = child.spawn("ffmpeg", [
+        "-re",
+        "-y",
+        "-i",
+        "udp://192.168.7.1:1234",
+        "-preset",
+        "ultrafast",
+        "-f",
+        "mjpeg",
+        "pipe:1"
+    ]);
+
+    ffmpeg.on('error', function (err) {
+        console.log(err);
+        throw err;
+    });
+
+    ffmpeg.on('close', function (code) {
+        console.log('ffmpeg exited with code ' + code);
+    });
+
+    ffmpeg.stderr.on('data', function (data) {
+        // Don't remove this
+        // Child Process hangs when stderr exceed certain memory
+    });
+
+    ffmpeg.stdout.on('data', function (data) {
+        var frame = Buffer.from(data).toString('base64'); //convert raw data to string
+        io.sockets.emit('canvas', frame); //send data to client
+    });
+});
+
+server.listen({ port }, () => {
+    console.log(`🚀 Server ready at http://0.0.0.0:${port}`);
+});
