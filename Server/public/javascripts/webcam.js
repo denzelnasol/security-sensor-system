363bf076de36341e74ff967f7f4c0f7a14e3a02a
denzelnasol <denzelnasol@gmail.com>
2023-03-18 15:43:16 -0700

    refactor server to use socket io with an express.js server

--- Server/public/javascripts/webcam.js
+++ Server/public/javascripts/webcam.js
@@ -1,8 +1,51 @@
-// Code adapted from the sample code from jsmpeg's GitHub:
-// https://github.com/phoboslab/jsmpeg/blob/master/view-stream.html 
-const WS_ADDR = "127.0.0.1";
-const WS_PORT = 8088;
-const options = {
-      canvas: $("#self-view")
- };
-new JSMpeg.Player(`ws://${WS_ADDR}:${WS_PORT}`, options);
+
+/**
+ * Code referenced from jsmpeg github issues by user jvictorsoto:
+ * https://github.com/phoboslab/jsmpeg/issues/199
+ */
+class JSMpegWritableSource {
+      constructor(url, options) {
+            this.destination = null;
+
+            this.completed = false;
+            this.established = false;
+            this.progress = 1;
+
+            // Streaming is obiously true when using a stream
+            this.streaming = true;
+      }
+
+      connect(destination) {
+            this.destination = destination;
+      }
+
+      start() {
+            this.established = true;
+            this.completed = true;
+            this.progress = 1;
+      }
+
+      resume() { // eslint-disable-line class-methods-use-this
+
+      }
+
+      destroy() { // eslint-disable-line class-methods-use-this
+      }
+
+      write(data) {
+            this.destination.write(data);
+      }
+}
+
+const socket = io.connect('http://192.168.7.1:8088');
+
+const canvas = $("#self-view")[0];
+
+const streamPlayer = new JSMpeg.Player(null, {
+      source: JSMpegWritableSource,
+      canvas
+});
+
+socket.on('stream', (data) => {
+      streamPlayer.source.write(data);
+});
--- Server/server.js
+++ Server/server.js
@@ -1,2 +1,59 @@
-import stream from "./lib/stream.js";
-stream.listen();
+// import stream from "./lib/stream.js";
+// stream.listen();
+/*
+    Code adapted from the sample code from jsmpeg's GitHub:
+    https://github.com/phoboslab/jsmpeg/blob/master/websocket-relay.js
+*/
+
+import fs from "fs";
+import dgram from "dgram";
+import path from "path";
+
+
+import express from 'express';
+import { Server as SocketIOServer } from 'socket.io';
+
+const __dirname = path.resolve();
+
+const STREAM_PORT = 8080;
+const STREAM_IP_ADDRESS = '192.168.7.1';
+const WEBSOCKET_PORT = 8088;
+const WEBSOCKET_IP_ADDRESS = '192.168.7.1';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+const server = app.listen(WEBSOCKET_PORT, WEBSOCKET_IP_ADDRESS, () => {
+    console.log(`server listening on port ${WEBSOCKET_PORT} with address`, server.address());
+})
+
+const io = new SocketIOServer(server);
+
+io.on('connection', (socket) => {
+    console.log('A client has connected');
+
+    // Handle disconnection of clients
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Initialize a file stream to send stream data to
+const fileStream = fs.createWriteStream(`${Date.now().toString()}.mp4`);
+
+// Initialize socket to listen for the webcam streaming data
+const udpServer = dgram.createSocket('udp4');
+udpServer.bind(STREAM_PORT, STREAM_IP_ADDRESS);
+
+// Send the data to the web socket to relay to clients
+// and create a file recording
+udpServer.on('message', (msg, rinfo) => {
+    // console.log("MESSAGES RECEIVED: ", msg);
+    io.emit('stream', msg);
+    fileStream.write(msg);
+});
