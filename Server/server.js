77d6e7e82ff511048c666b57184932f7703f6ea3
Mathew <matheww@sfu.ca>
2023-03-22 16:33:32 -0700

    add analytics, api/websocket endpoints and express server

--- Server/server.js
+++ Server/server.js
@@ -2,16 +2,14 @@
     Code adapted from the sample code from jsmpeg's GitHub:
     https://github.com/phoboslab/jsmpeg/blob/master/websocket-relay.js
 */
-import fs from "fs";
-import dgram from "dgram";
-import path from "path";
-import express from 'express';
-import { Server as SocketIOServer } from 'socket.io';
+var fs   = require('fs');
+var path = require('path');
+var express = require('express');
 
-const __dirname = path.resolve();
+var analyzer = require('./lib/analyzer');
+var sioserver = require('./lib/udp_server');
+// var sioserver = require('./lib/socketio_server');
 
-const STREAM_PORT = 8080;
-const STREAM_IP_ADDRESS = '192.168.7.1';
 const WEBSOCKET_PORT = 8088;
 const WEBSOCKET_IP_ADDRESS = '192.168.7.1';
 
@@ -35,6 +33,14 @@ app.get('/recordings', function (req, res) {
     });
 });
 
+app.get('/data', function (req, res) {
+    res.json(analyzer.getDataPoints());
+});
+app.post('/data', function (req, res) {
+    analyzer.addDataPoint(req.body);
+    res.sendStatus(201);
+});
+
 
 app.get('/recordings/:id', function (req, res) {
     const filePath = path.join(__dirname, `recordings/${req.params.id}`);
@@ -47,53 +53,5 @@ const server = app.listen(WEBSOCKET_PORT, WEBSOCKET_IP_ADDRESS, () => {
     console.log(`server listening on port ${WEBSOCKET_PORT} with address`, server.address());
 })
 
-const io = new SocketIOServer(server);
-
-io.sockets.on('connection', (socket) => {
-    console.log('A client has connected');
-
-    // Handle disconnection of clients
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
-});
-
-// Initialize a file stream to send stream data to
-let fileStream;
-
-// Initialize socket to listen for the webcam streaming data
-const udpServer = dgram.createSocket('udp4');
-udpServer.bind(STREAM_PORT, STREAM_IP_ADDRESS);
-
-// Send the data to the web socket to relay to clients
-// and create a file recording
-udpServer.on('message', (msg, rinfo) => {
-    if (!fileStream) {
-        /**
-         * Source to get date
-         * https://stackoverflow.com/questions/12409299/how-to-get-current-formatted-date-dd-mm-yyyy-in-javascript-and-append-it-to-an-i
-         */
-        const today = new Date();
-        const year = today.getFullYear();
-        const month = today.getMonth() + 1; // Months start at 0!
-        const day = today.getDate();
-        const minutes = today.getMinutes();
-        const seconds = today.getSeconds();
-        const formattedToday = year + ":" + month + ":" + day + ":" + minutes + ":" + seconds;
-        fileStream = fs.createWriteStream(`recordings/${formattedToday}.mp4`);
-    } else {
-        fileStream.write(msg);
-    }
-
-    io.emit('stream', msg);
-});
-
-udpServer.on('close', () => {
-    if (fileStream) {
-        fileStream.end();
-    }
-});
-
-udpServer.on('error', (err) => {
-    console.log(`server error: ${err.stack}`);
-});
+// set up socket io for sending data to clients
+sioserver.listen(server);
