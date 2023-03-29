18edee0ce49f51a2e3e33cd31d71d401d189bd0b
Harry Nguyen <harry@bulb.localdomain>
2023-03-29 08:57:37 -0700

    add more dependencies to startup script

--- startup.sh
+++ startup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# install packages
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt install -y connman
-
 # turn off and on wifi to make sure wifi adapter works
+# This assumes wifi has been previously connected to before
 connmanctl disable wifi
 sleep 1
 connmanctl enable wifi
+
+# install packages
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt install -y connman
+sudo apt-get install -y libopencv-dev
+sudo apt-get install -y ffmpeg
 
 # start up camera app
 path_to_app="/mnt/remote/myApps"
