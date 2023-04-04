b24222146ffe3367ac43841fd5b88000b3f34090
Mathew <mathew@sfu.ca>
2023-04-04 22:38:01 +0000

    fixed udp streaming on udp

--- Server/lib/udp_server.js
+++ Server/lib/udp_server.js
@@ -8,7 +8,7 @@ var udpServer;
 var fileStream;
 
 const STREAM_PORT = 8080;
-const STREAM_IP_ADDRESS = '192.168.7.1';
+const STREAM_IP_ADDRESS = 'localhost';
 
 exports.listen = function(server) {
     io = new SocketIOServer(server);
@@ -24,12 +24,13 @@ exports.listen = function(server) {
 
     // Initialize socket to listen for the webcam streaming data
     udpServer = dgram.createSocket('udp4');
-    udpServer.bind(STREAM_PORT, STREAM_IP_ADDRESS);
+    udpServer.bind(STREAM_PORT);
 
     // Send the data to the web socket to relay to clients
     // and create a file recording
     udpServer.on('message', (msg, rinfo) => {
-        if (!fileStream) {
+//       console.log("received: " + msg.toString());
+	 if (!fileStream) {
             fileStream = fs.createWriteStream(`recordings/${formatDate()}.mp4`);
         } else {
             fileStream.write(msg);
